110229e3d950815b2054085b49f6c4b96c4c83eb
Z.Raslan <zikaraslan23@gmail.com>
2025-02-13 14:55:55 +0200

    End Of the Layout Section

---
+++ LayoutViewSection/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewSection.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Route("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ LayoutViewSection/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewSection.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("all-products")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("product-search/{productId?}")]
+        public IActionResult Search(int? productId)
+        {
+            ViewBag.ProductId = productId;
+            return View();
+        }
+        [Route("product-order")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+
+
+
+    }
+}
