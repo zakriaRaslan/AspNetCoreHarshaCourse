b571b0eb8904db4ccdb5c2c677e1b71e9c400d9d
Z.Raslan <zikaraslan23@gmail.com>
2025-02-15 21:34:28 +0200

    Add Parameter To ViewComponent Invoke Method And Send Data From The View To Display a different data in the view component

--- 08-ViewComponentSection/ViewComponents/GridViewComponent.cs
+++ 08-ViewComponentSection/ViewComponents/GridViewComponent.cs
@@ -7,22 +7,12 @@ namespace _08_ViewComponentSection.ViewComponents
     //[ViewComponent]
     public class GridViewComponent : ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync(GridPersonModel grid)
         {
-            GridPersonModel gridPersonModel = new GridPersonModel()
-            {
-                GridTitle = "Persons List",
-                Persons = new List<Person>()
-                {
-                    new Person(){Name="Sayed" , JopTitle="Back-end Developer"},
-                    new Person(){Name="Yahia" , JopTitle="Full-stack Developer"},
-                    new Person(){Name="Mariam" , JopTitle="Front-end Developer"},
-                    new Person(){Name="Mostafa" , JopTitle="Mobile Developer"},
-                }
-            };
+          
 
             //ViewData["Grid"] = gridPersonModel;
-            return View(gridPersonModel);// this will search for the view in 
+            return View(grid);// this will search for the view in 
             // Views/Shared/Component/{name of the Class = Grid}/Default.cshtml.
             // to change the name of the view use
             // return View("Name Of View")
