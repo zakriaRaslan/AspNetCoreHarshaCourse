55d61d963f55c49e148c0d311a9910a9db3dd9b1
Z.Raslan <zikaraslan23@gmail.com>
2025-02-18 01:13:45 +0200

    Using Autofac

--- 09-DIsection/Program.cs
+++ 09-DIsection/Program.cs
@@ -1,14 +1,36 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using ServicesContract;
 using ServicesLayer;
 
 var builder = WebApplication.CreateBuilder(args);
+// you need to Add Autofac & Autofac.Extensions From Nuget Packages
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Services.AddControllersWithViews();
 //builder.Services.Add(new ServiceDescriptor(
 //    typeof(ICitiesService),
 //    typeof(CitiesService),
 //    ServiceLifetime.Transient
 //    ));
-builder.Services.AddScoped<ICitiesService , CitiesService>();
+//builder.Services.AddScoped<ICitiesService , CitiesService>();
+
+#region Using Autofac To Register Services
+
+builder.Host.ConfigureContainer<ContainerBuilder>(container =>
+{
+    // this like AddTransient
+    //container.RegisterType<CitiesService>().As<ICitiesService>().InstancePerDependency();
+
+    // this like AddScopped
+    container.RegisterType<CitiesService>().As<ICitiesService>().InstancePerLifetimeScope();
+
+    //this like AddSingelton
+    //container.RegisterType<CitiesService>().As<ICitiesService>().SingleInstance();
+});
+
+#endregion
+
+
 var app = builder.Build();
 
 app.UseStaticFiles();
