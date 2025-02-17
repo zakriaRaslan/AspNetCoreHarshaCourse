ce3adf60c05183ffb00cddc660c79fe773c260c0
Z.Raslan <zikaraslan23@gmail.com>
2025-02-17 18:32:19 +0200

    Make the DIsection project structure and the ServiceLayer To Make A service to get a list of cities without using the dependecy inversion principle

---
+++ 09-DIsection/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServicesLayer;
+namespace _09_DIsection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly CitiesService _citiesService;
+
+       public HomeController()
+        {
+            _citiesService = new CitiesService();
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            List<string> citites = _citiesService.GetCitites();
+            return View(citites);
+        }
+    }
+}
---
+++ ServicesLayer/CitiesService.cs
@@ -0,0 +1,22 @@
+﻿namespace ServicesLayer
+{
+    public class CitiesService
+    {
+        private List<string> _cities;
+        public CitiesService()
+        {
+            _cities = new List<string>()
+            {
+                "Cairo",
+                "Alexandria",
+                "Qalybia",
+                "Luxor",
+                "Sinaa"
+            };
+        }
+
+        public List<string> GetCitites() { 
+        return _cities;
+        }
+    }
+}
