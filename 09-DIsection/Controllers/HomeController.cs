efb84a154ab4620e17e65112cdf89d1d1c8cc56f
Z.Raslan <zikaraslan23@gmail.com>
2025-02-17 22:57:07 +0200

    Inject Service To A Method Directly Without Using Constructor

--- 09-DIsection/Controllers/HomeController.cs
+++ 09-DIsection/Controllers/HomeController.cs
@@ -5,18 +5,27 @@ namespace _09_DIsection.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ICitiesService _citiesService;
+       // private readonly ICitiesService _citiesService;
 
-       public HomeController(ICitiesService citiesService)
-        {
-            _citiesService = citiesService;
-        }
+       //public HomeController(ICitiesService citiesService)
+       // {
+       //     _citiesService = citiesService;
+       // }
 
+       // [Route("/")]
+       // public IActionResult Index()
+       // {
+       //     List<string> citites = _citiesService.GetCities();
+       //     return View(citites);
+       // }
+
+        #region Add Service To Method directly 
         [Route("/")]
-        public IActionResult Index()
+        public IActionResult Index([FromServices] ICitiesService _citiesService)
         {
             List<string> citites = _citiesService.GetCities();
             return View(citites);
         }
+        #endregion
     }
 }
